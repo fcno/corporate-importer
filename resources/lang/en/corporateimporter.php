55a5b1a86fa21ca0591dbb873faa84a6d6c70a9f
fcno <fabiocassiano@jfes.jus.br>
2022-01-07 18:25:13 -0300

    add: Localization

---
+++ resources/lang/en/corporateimporter.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    /**
+     * Message sets used in the package
+     *
+     * Locale: default/en
+     *
+     * @see https://laravel.com/docs/8.x/localization
+     */
+    'filenotreadableexception' => 'The file entered could not be read!',
+    'validation' => 'Validation failed!',
+];
---
+++ resources/lang/pt-br/corporateimporter.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    /**
+     * Message sets used in the package
+     *
+     * Locale: pt-br
+     *
+     * @see https://laravel.com/docs/8.x/localization
+     */
+    'filenotreadableexception' => 'O arquivo informado não pôde ser lido!',
+    'validation' => 'Validação falhou!',
+];

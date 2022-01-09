a681d49ee931ed006b652e1abbcca3682fde1ec7
fcno <fabiocassiano@jfes.jus.br>
2022-01-09 01:27:58 -0300

    add: importador da entidade usuário

---
+++ tests/Feature/Importer/Usuario/ExceptionTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use Fcno\CorporateImporter\Exceptions\FileNotReadableException;
+use Fcno\CorporateImporter\Importer\UsuarioImporter;
+
+test('lança exceção ao executar a importação com arquivo inválido', function ($file_name) {
+    expect(
+        fn () => UsuarioImporter::make()
+                                ->from($file_name)
+                                ->execute()
+    )->toThrow(FileNotReadableException::class);
+})->with([
+    'inexistente.xml', // inexistente
+    '',                // falso boleano
+]);
---
+++ tests/Feature/Importer/Usuario/RegularTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use Fcno\CorporateImporter\Importer\CargoImporter;
+use Fcno\CorporateImporter\Importer\FuncaoImporter;
+use Fcno\CorporateImporter\Importer\LotacaoImporter;
+use Fcno\CorporateImporter\Importer\UsuarioImporter;
+use Fcno\CorporateImporter\Models\Usuario;
+use Illuminate\Support\Facades\Log;
+
+test('make retorna o objeto da classe', function () {
+    expect(UsuarioImporter::make())->toBeInstanceOf(UsuarioImporter::class);
+});
+
+test('consegue importar os usuários do arquivo corporativo', function () {
+    // forçar a execução de duas queries em pontos distintos e testá-las
+    config(['corporateimporter.maxupsert' => 2]);
+
+    CargoImporter::make()
+                    ->from($this->file_system->path($this->file_name))
+                    ->execute();
+    FuncaoImporter::make()
+                    ->from($this->file_system->path($this->file_name))
+                    ->execute();
+    LotacaoImporter::make()
+                    ->from($this->file_system->path($this->file_name))
+                    ->execute();
+    UsuarioImporter::make()
+                    ->from($this->file_system->path($this->file_name))
+                    ->execute();
+    $usuarios = Usuario::get();
+
+    expect($usuarios)->toHaveCount(5)
+    ->and($usuarios->pluck('nome'))->toMatchArray(['Pessoa 1', 'Pessoa 2', 'Pessoa 3', 'Pessoa 4', 'Pessoa 5']);
+});
+
+test('cria os logs para os usuários inválidos', function () {
+    CargoImporter::make()
+        ->from($this->file_system->path($this->file_name))
+        ->execute();
+    FuncaoImporter::make()
+        ->from($this->file_system->path($this->file_name))
+        ->execute();
+    LotacaoImporter::make()
+        ->from($this->file_system->path($this->file_name))
+        ->execute();
+
+    Log::shouldReceive('log')
+        ->times(13)
+        ->withArgs(
+            function ($level) {
+                return $level === 'warning';
+            }
+        );
+
+    UsuarioImporter::make()
+        ->from($this->file_system->path($this->file_name))
+        ->execute();
+
+    expect(Usuario::count())->toBe(5);
+});

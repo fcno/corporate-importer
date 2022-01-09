4d58b861f415971b70158102ae7d451cdbc32500
fcno <fabiocassiano@jfes.jus.br>
2022-01-09 20:59:38 -0300

    add: testar o a criação dos logs de inicio e fim

--- tests/Feature/Importer/CorporateImporterTest.php
+++ tests/Feature/Importer/CorporateImporterTest.php
@@ -11,6 +11,7 @@ use Fcno\CorporateImporter\Models\Cargo;
 use Fcno\CorporateImporter\Models\Funcao;
 use Fcno\CorporateImporter\Models\Lotacao;
 use Fcno\CorporateImporter\Models\Usuario;
+use Illuminate\Support\Facades\Log;
 
 test('facade retorna objeto da classe', function () {
     expect(CorporateImporter::from($this->file_path))->toBeInstanceOf(Importer::class);
@@ -25,7 +26,68 @@ test('lança exceção ao executar a importação com arquivo inválido', functi
     '',                // falso boleano
 ]);
 
-test('importa a estrutura corporativa completa', function () {
+test('cria apenas os logs de validação se o package for configurado para não logar', function () {
+    config(['corporateimporter.logging' => false]);
+
+    $infos
+        = 0  // início da importação
+        + 0; // fim da importação
+    $warnings
+        = 6   // cargos inválidos
+        + 6   // funções inválidas
+        + 18  // lotações inválidas
+        + 13; // fim da importação
+
+    Log::shouldReceive('log')
+        ->times($infos)
+        ->withArgs(
+            function ($level) {
+                return $level === 'info';
+            }
+        );
+
+    Log::shouldReceive('log')
+        ->times($warnings)
+        ->withArgs(
+            function ($level) {
+                return $level === 'warning';
+            }
+        );
+
+    CorporateImporter::from($this->file_path)->run();
+
+    expect(Cargo::count())->toBe(3)
+    ->and(Funcao::count())->toBe(3)
+    ->and(Lotacao::count())->toBe(5)
+    ->and(Usuario::count())->toBe(5);
+});
+
+test('importa a estrutura corporativa completa e cria todos os logs', function () {
+    $infos
+        = 1  // início da importação
+        + 1; // fim da importação
+    $warnings
+        = 6   // cargos inválidos
+        + 6   // funções inválidas
+        + 18  // lotações inválidas
+        + 13; // fim da importação
+
+    Log::shouldReceive('log')
+        ->times($infos)
+        ->withArgs(
+            function ($level) {
+                return $level === 'info';
+            }
+        );
+
+    Log::shouldReceive('log')
+        ->times($warnings)
+        ->withArgs(
+            function ($level) {
+                return $level === 'warning';
+            }
+        );
+
     CorporateImporter::from($this->file_path)->run();
 
     expect(Cargo::count())->toBe(3)

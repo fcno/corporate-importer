0d2b72bf982a433ff64d030336987e8e3647522a
fcno <fabiocassiano@jfes.jus.br>
2022-01-09 20:28:57 -0300

    add: string para usar no início/fim da importação

--- resources/lang/en/corporateimporter.php
+++ resources/lang/en/corporateimporter.php
@@ -9,6 +9,8 @@ return [
      *
      * @see https://laravel.com/docs/8.x/localization
      */
+    'end' => 'End of corporate structure import',
     'filenotreadableexception' => 'The file entered could not be read!',
+    'start' => 'Start of corporate structure import',
     'validation' => 'Validation failed!',
 ];
--- resources/lang/pt-br/corporateimporter.php
+++ resources/lang/pt-br/corporateimporter.php
@@ -9,6 +9,8 @@ return [
      *
      * @see https://laravel.com/docs/8.x/localization
      */
+    'end' => 'Fim da importação da estrutura corporativa',
     'filenotreadableexception' => 'O arquivo informado não pôde ser lido!',
+    'start' => 'Início da importação da estrutura corporativa',
     'validation' => 'Validação falhou!',
 ];

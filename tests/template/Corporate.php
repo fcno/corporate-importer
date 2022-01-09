4e87e961cca98be6b5277f7a70e075b595280b6b
fcno <fabiocassiano@jfes.jus.br>
2022-01-09 01:03:46 -0300

    add: pessoas para testar a importação

--- tests/template/Corporate.php
+++ tests/template/Corporate.php
@@ -71,5 +71,40 @@ return <<<XML
         <!-- Sigla com mais de 55 caracteres -->
         <lotacao id="14" nome="Lotação 14" sigla="X7mnMeXqKVHHDsUqfs6nnGjWVi33A0AfziN8Z9ALD2GiQEgltl9Ucd45" idPai="1"/>
     </lotacoes>
+    <pessoas>
+         <!-- Pessoas Válidas -->
+        <pessoa id="1" nome="Pessoa 1" sigla="Sigla 1" cargo="1" lotacao="2" funcaoConfianca="1"/>
+        <pessoa id="2" nome="Pessoa 2" sigla="Sigla 2" cargo="1" lotacao="2" funcaoConfianca="2"/>
+        <pessoa id="3" nome="Pessoa 3" sigla="Sigla 3" cargo="1" lotacao="3" funcaoConfianca="2"/>
+        <pessoa id="4" nome="Pessoa 4" sigla="Sigla 4" cargo="2" lotacao="3" funcaoConfianca=""/>
+        <pessoa id="5" nome="Pessoa 5" sigla="Sigla 5" cargo="2" lotacao="3" funcaoConfianca=""/>
+        <!-- Pessoas Inválidas -->
+        <!-- Sem o nome -->
+        <pessoa id="6" sigla="Sigla 6" cargo="1" lotacao="2" funcaoConfianca=""/>
+        <!-- Nome nulo/vazio -->
+        <pessoa id="7" nome="" sigla="Sigla 7" cargo="1" lotacao="2" funcaoConfianca=""/>
+        <!-- Nome com mais de 255 caracteres -->
+        <pessoa id="8" nome="F8WRcUYFTJKuA0ZJva5MX5FX7mnMeXqKVHHDsUqfs6nnGjWVi33A0AfziN8Z9ALD2GiQEgltl9Ucd45rs2TSA2jvqeLT2t60X5S3O4lt8j9ZURmSPm1LBvLXWcBmh4vLjORDuSH50SBcwzcZbaTTbxFqZI5k9LOqcw5VlRtqCox44sAFXKkKVms9KvnL9ltmvtRp63JxqpWrp81TuYcnXuPc2MALKWO9Dwxc8pxP6XWKsJ5M3qYVgib8OIEc00O4" sigla="Sigla 8" cargo="1" lotacao="2" funcaoConfianca=""/>
+        <!-- Sem o sigla -->
+        <pessoa id="9" nome="Pessoa 9" cargo="1" lotacao="2" funcaoConfianca=""/>
+        <!-- Sigla nulo/vazio -->
+        <pessoa id="10" nome="Pessoa 10" sigla="" cargo="1" lotacao="2" funcaoConfianca=""/>
+        <!-- Sigla com mais de 20 caracteres -->
+        <pessoa id="11" nome="Pessoa 11" sigla="TJKuA0ZJva5MX5FX7mnMe" cargo="1" lotacao="2" funcaoConfianca=""/>
+        <!-- Sem cargo -->
+        <pessoa id="12" nome="Pessoa 12" sigla="Sigla 12" lotacao="2" funcaoConfianca=""/>
+        <!-- Cargo nulo -->
+        <pessoa id="13" nome="Pessoa 13" sigla="Sigla 13" cargo="" lotacao="2" funcaoConfianca=""/>
+        <!-- Cargo inexistente -->
+        <pessoa id="14" nome="Pessoa 14" sigla="Sigla 14" cargo="1000" lotacao="2" funcaoConfianca=""/>
+        <!-- Sem lotação -->
+        <pessoa id="15" nome="Pessoa 15" sigla="Sigla 15" cargo="1" funcaoConfianca=""/>
+        <!-- Lotação nula -->
+        <pessoa id="16" nome="Pessoa 16" sigla="Sigla 16" cargo="1" lotacao="" funcaoConfianca=""/>
+        <!-- Lotação inexistente -->
+        <pessoa id="17" nome="Pessoa 17" sigla="Sigla 17" cargo="1" lotacao="1000" funcaoConfianca=""/>
+        <!-- Função inexistente -->
+        <pessoa id="18" nome="Pessoa 18" sigla="Sigla 18" cargo="1" lotacao="2" funcaoConfianca="1000"/>
+    </pessoas>
 </base>
 XML;

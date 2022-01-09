a23865c398e480aa819c121ac9bb437cf2fcc439
fcno <fabiocassiano@jfes.jus.br>
2022-01-09 16:03:18 -0300

    fix: inline array

--- src/Models/Usuario.php
+++ src/Models/Usuario.php
@@ -16,13 +16,7 @@ class Usuario extends Model
 
     protected $table = 'usuarios';
 
-    protected $fillable = [
-        'sigla',
-        'nome',
-        'lotacao_id',
-        'cargo_id',
-        'funcao_id'
-    ];
+    protected $fillable = ['sigla', 'nome', 'lotacao_id', 'cargo_id', 'funcao_id'];
 
     /**
      * Lotação de um determinado usuário.
